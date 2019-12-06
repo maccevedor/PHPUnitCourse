09e1f9694d91e2ffd028e38b83cd5f9305a1b711
unknown <maccevedor@gmail.com>
2019-12-06 16:49:55 -0500

    video 22

--- src/User.php
+++ src/User.php
@@ -21,6 +21,27 @@ class User
     public $surname;
 
     /**
+     * Email address
+     * @var string
+     */
+    public $email;
+    
+    /**
+     * Mailer object
+     * @var Mailer
+     */
+    protected $mailer;
+    
+    /**
+     * Set the mailer dependency
+     *
+     * @param Mailer $mailer The Mailer object
+     */
+    public function setMailer(Mailer $mailer) {
+        $this->mailer = $mailer;        
+    }    
+        
+    /**
      * Get the user's full name from their first name and surname
      *
      * @return string The user's full name
@@ -29,4 +50,16 @@ class User
     {
         return trim("$this->first_name $this->surname");
     }
+    
+    /**
+     * Send the user a message
+     *
+     * @param string $message The message
+     *
+     * @return boolean True if sent, false otherwise
+     */
+    public function notify($message)
+    {
+        return $this->mailer->sendMessage($this->email, $message);        
+    }    
 }
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -4,6 +4,7 @@ use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
+
     public function testReturnsFullName()
     {
         $user = new User;
@@ -20,4 +21,20 @@ class UserTest extends TestCase
 
         $this->assertEquals('', $user->getFullName());
     }
+    
+    public function testNotificationIsSent()
+    {
+        $user = new User;
+        
+        $mock_mailer = $this->createMock(Mailer::class);
+        
+        $mock_mailer->method('sendMessage')
+                    ->willReturn(true);        
+                
+        $user->setMailer($mock_mailer);
+                        
+        $user->email = 'dave@example.com';
+        
+        $this->assertTrue($user->notify("Hello"));
+    }    
 }

6bfb4dae7c4d4663814d5ef13e09d6da957835b5
unknown <maccevedor@gmail.com>
2019-12-09 08:51:54 -0500

    Video 26 exmaple

--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -4,6 +4,10 @@ use PHPUnit\Framework\TestCase;
 
 class OrderTest extends TestCase
 {
+    public function tearDown(): void 
+    {
+        Mockery::close();
+    }
     public function testOrderIsProcessed()
     {
         $gateway = $this->getMockBuilder('PaymentGateway')
@@ -20,5 +24,22 @@ class OrderTest extends TestCase
         $order->amount = 200;
         
         $this->assertTrue($order->process());
+    }
+
+    public function testOrderIsProcessedUsingMockery()
+    {
+        $gateway = Mockery::mock('PaymentGateway');
+
+        $gateway->shouldReceive('charge')
+                ->once()
+                ->with(200)
+                ->andReturn(true);
+
+        $order = new Order($gateway);
+
+        $order->amount = 200;
+        
+        $this->assertTrue($order->process());
+
     }
 }
